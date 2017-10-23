c8c334b8d34fa83a5f20a9170aabb7215930610e
truyenthegioi_com <truyenthegioi_com@packer-debian-8-amd64.droplet.local>
2017-10-23 02:45:28 +0000

    new update

--- tools/wetruyen-com.sh
+++ tools/wetruyen-com.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-php F:/websites/wt/tools/wetruyen-com.sh
+#php F:/websites/wt/tools/wetruyen-com.sh
 
-#while true
-#do
-# php F:/websites/wt/tools/wetruyen-com.sh
-# sleep 60
-#done
+while true
+do
+	# php  /home/truyenthegioi_com/public_html/tools/webtruyen-com.php
+  curl http://truyenthegioi.com/thuypx_import/story
+ sleep 1
+done
