d6066b490efdfd602875acdc147eb0899492527a
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-10-26 14:37:48 +0700

    install google recaptcha

--- sites/all/themes/oms/templates/footer.tpl.php
+++ sites/all/themes/oms/templates/footer.tpl.php
@@ -6,4 +6,11 @@
       </div>
     </div>
   </div>
+    <div id="footer-bottom" class="footer-bottom">
+        <div class="container">
+            <div class="row">
+              <?php print render($page['footer_bottom']) ?>
+            </div>
+        </div>
+    </div>
 </footer>
