22081b43041fa328bc161ae2f839b13daab8bea7
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-11-02 08:40:09 +0700

    crawl

--- tools/config.php
+++ tools/config.php
@@ -6,5 +6,5 @@ include "libs/helper.php";
 //database connection
 include "libs/database/MysqliDb.php";
 global $db;
-$db = new MysqliDb ('localhost','truyenthegioi_co', 'Sta9D3#23tM9aj','truyenthegioi_com',3306,'utf8');
+$db = new MysqliDb ('localhost','root', 'root','wt',3306,'utf8');
 
---
+++ tools/crawl-story.php
@@ -0,0 +1,35 @@
+<?php
+//https://github.com/joshcam/PHP-MySQLi-Database-Class
+include 'config.php';
+$helper=new helper();
+$domain='webtruyen.com';
+
+$url='http://webtruyen.com/all/';
+
+
+$html_str =$helper->curl_download($url);
+$html = new simple_html_dom();
+$html->load($html_str);
+$db = MysqliDb::getInstance();
+foreach($html->find('.list-content .list-caption a') as $a)
+{
+    $db->where('url_source',$a->href);
+    $row=$db->getOne('crawl_story');
+    if($row){
+        echo "Exist record".PHP_EOL;
+    }else{
+        $title=$helper->cleanTitle($a->plaintext);
+        $title_code=$helper->encodeTitle($a->plaintext);
+
+        $db->where('title_code',$title_code);
+        $row=$db->getOne('crawl_story');
+        $data=array(
+            'domain'=>$domain,
+            'url_source'=>$a->href,
+            'title' => $title,
+            'title_code'=>$title_code,
+        );
+        $db->insert('crawl_story',$data);
+    }
+}
+echo 'done';
