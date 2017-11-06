88816f96a6c99cbb1a3459ff7f8af9d22fb7cb98
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-11-06 17:22:04 +0700

    AM

--- tools/webtruyen-com.php
+++ tools/webtruyen-com.php
@@ -39,5 +39,42 @@ foreach($html->find('.list-content .list-caption a') as $a)
       $db->insert('crawl_story',$data);
   }
 }
+
+
+$url='http://webtruyen.com/all/2/';
+
+$html_str =$helper->curl_download($url);
+$html = new simple_html_dom();
+$html->load($html_str);
+$db = MysqliDb::getInstance();
+foreach($html->find('.list-content .list-caption a') as $a)
+{
+  $data=array();
+  $db->where('url_source',$a->href);
+  $row=$db->getOne('crawl_story');
+  if($row){
+    echo "Exist record".PHP_EOL;
+    $db->where ('id',$row['id']);
+    $db->update('crawl_story',array('status'=>2));
+
+  }else{
+    $title=$helper->cleanTitle($a->plaintext);
+    $title_code=$helper->encodeTitle($a->plaintext);
+
+    $db->where('title_code',$title_code);
+    $row=$db->getOne('crawl_story');
+    $data=array(
+      'domain'=>$domain,
+      'url_source'=>$a->href,
+      'title' => $title,
+      'title_code'=>$title_code,
+    );
+    if($row){
+      $data['duplicate']=1;
+    }
+    $db->insert('crawl_story',$data);
+  }
+}
+
 echo 'done';
 
